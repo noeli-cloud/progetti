bb915ccaa82c423daa62b0ed6b6bbd510b6f7e19
modestemax <modestemax@gmail.com>
2019-03-17 04:12:50 +0100

    project item detail

---
+++ force-app/main/default/lwc/projectItemDetail/projectItemDetail.js
@@ -0,0 +1,13 @@
+import { LightningElement/*,track,wire*/ } from 'lwc';
+// import getProject from '@salesforce/apex/ProjectController.getProjects';
+
+
+
+export default class ProjectItemDetail extends LightningElement {
+
+
+// @track projectId;
+// @wire(getProject,{projectId:'$projectId'})project;
+
+
+}
