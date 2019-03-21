3e11e54e649f1b72540b09ddb9f9879afe28b923
modestemax <modestemax@gmail.com>
2019-03-21 10:01:46 +0100

    ...

--- force-app/main/default/aura/projectStatus/projectStatusController.js
+++ force-app/main/default/aura/projectStatus/projectStatusController.js
@@ -1,18 +1,15 @@
 ({
     handlePubsubReady: function (component) {
-        debugger
         var pubsub = component.find('pubsub');
         var callback = $A.getCallback(function (projectId) {
             component.set('v.recordId', projectId);
             var picklistPath = component.find('picklistPath');
-            debugger
             // service.reloadRecord();
         });
         pubsub.registerListener('projectselected', callback);
     },
 
     handleDestroy: function (component) {
-        debugger
         var pubsub = component.find('pubsub');
         pubsub.unregisterAllListeners();
     },
--- force-app/main/default/lwc/projectItemDetail/projectItemDetail.js
+++ force-app/main/default/lwc/projectItemDetail/projectItemDetail.js
@@ -1,5 +1,6 @@
 import { LightningElement, track, wire } from 'lwc';
-// import getProject from '@salesforce/apex/ProjectController.getProject';
+import getProjectBenefits from '@salesforce/apex/ProjectController.getProjectBenefits';
+
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
 
 import { CurrentPageReference } from 'lightning/navigation';
@@ -10,17 +11,25 @@ export default class ProjectItemDetail extends LightningElement {
 
     @track projectId;
     @wire(CurrentPageReference) pageRef;
+    @track _benefits;
+    @wire(getProjectBenefits, { projectId: '$projectId' })
+    getProjectBenefits({ data, error }) {
+        if (data) {
+            this._benefits = data
+        }
+
+    }
 
     connectedCallback() {
         registerListener('projectselected', this.handleProjectSelected, this);
-        alert('max 7.33')
+        // alert('max 7.36')
     }
 
     disconnectedCallback() {
         unregisterAllListeners(this);
     }
 
-    handleProjectSelected(projectId) {        
+    handleProjectSelected(projectId) {
         this.projectId = projectId;
     }
 
