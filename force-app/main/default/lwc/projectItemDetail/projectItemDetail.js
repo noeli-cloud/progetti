9fa1afaa14a20feb172fe7ec4382440122bebbc8
modestemax <modestemax@gmail.com>
2019-03-21 06:04:33 +0100

    ..

--- force-app/main/default/lwc/projectItemDetail/projectItemDetail.js
+++ force-app/main/default/lwc/projectItemDetail/projectItemDetail.js
@@ -13,7 +13,7 @@ export default class ProjectItemDetail extends LightningElement {
 
     connectedCallback() {
         registerListener('projectselected', this.handleProjectSelected, this);
-        alert('max 7.17')
+        alert('max 7.20')
     }
 
     disconnectedCallback() {
