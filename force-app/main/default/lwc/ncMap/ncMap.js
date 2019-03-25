be612fc8d9779add0c323a2dbe2788becfe1eb1d
modestemax <modestemax@gmail.com>
2019-03-25 02:53:11 +0100

    ...

--- force-app/main/default/lwc/ncMap/ncMap.js
+++ force-app/main/default/lwc/ncMap/ncMap.js
@@ -7,7 +7,7 @@ import { NavigationMixin } from 'lightning/navigation';
 export default class NcMap extends NavigationMixin(LightningElement) {
 
     renderedCallback() {
-        alert("2.9")
+        alert("2.18")
         debugger
         if (this.leaftletjsInitialized) {
             return;
@@ -28,7 +28,9 @@ export default class NcMap extends NavigationMixin(LightningElement) {
     initMap() {
         debugger;
         let m = this.template.querySelector('.map');
-        var map = L.map(m, { zoomControl: false }).setView([37.784173, -122.401557], 14);
+        var map = L.map(m, { zoomControl: true, zoom: 1 })
+            .setView([41.871941, 12.567380], 1)
+            // .zoomOut().zoomOut().zoomOut();
 
         L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
             {
@@ -36,10 +38,24 @@ export default class NcMap extends NavigationMixin(LightningElement) {
             }).addTo(map);
 
         // Add marker
-        const marker = L.marker([37.784173, -122.401557])
+        const marker = L.marker([41.871941, 12.567380])
         marker.addTo(map)
             .bindPopup('Home of Dreamforce');
+
+
+        marker.on('mouseover', () => {
+            marker.openPopup();
+        });
+
         marker.on('click', (ev) => {
+            debugger
+            this.navigateToList() // ev is an event object (MouseEvent in this case)
+        });
+
+        const marker2 = L.marker([-14.235004, -51.925282])
+        marker2.addTo(map)
+            .bindPopup('Home of Dreamforce');
+        marker2.on('click', (ev) => {
             debugger
             this.navigateToList() // ev is an event object (MouseEvent in this case)
         });
