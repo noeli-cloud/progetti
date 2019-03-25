81a789eba187abccb36327b179af96dcbe2b799e
modestemax <modestemax@gmail.com>
2019-03-25 03:33:55 +0100

    ....

---
+++ force-app/main/default/lwc/ncMap/mapData.js
@@ -0,0 +1,12 @@
+export default  {
+    brasil: { filter: '00B1i000000ZPJDEA4', latitude: -14.235004, longitude: -51.925282 },
+    italy: { filter: '00B1i000000ZPJ8EAO', latitude: 41.871941, longitude: 12.567380 },
+    iberia: { filter: '00B1i000000ZPJJEA4', latitude: 38.087349, longitude: -92.297142 },
+    peru: { filter: '00B1i000000ZPJKEA4', latitude: -9.189967, longitude: -75.015152 },
+    portugal: { filter: '00B1i000000ZPJNEA4', latitude: 39.399872, longitude: -8.224454 },
+    rusia: { filter: '00B1i000000ZPJIEA4', latitude: 8.095550, longitude: -75.392479 },
+    spain: { filter: '00B1i000000ZPJDEA4', latitude: 40.463669, longitude: -3.749220 },
+    argentina: { filter: '00B1i000000ZPJXEA4', latitude: -38.416096, longitude: -63.616673 },
+    chile: { filter: '00B1i000000ZPJLEA4', latitude: -35.675148, longitude: -71.542969 },
+    colombia: { filter: '00B1i000000ZPJTEA4', latitude: 4.570868, longitude: -74.297333 }
+}
--- force-app/main/default/lwc/ncMap/ncMap.js
+++ force-app/main/default/lwc/ncMap/ncMap.js
@@ -1,13 +1,15 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import LEAFTLET_JS from '@salesforce/resourceUrl/leaflet';
 import { NavigationMixin } from 'lightning/navigation';
-
+import getCountByCountry from '@salesforce/apex/ProjectController.getCountByCountry';
+import mapData from './mapData'
 
 export default class NcMap extends NavigationMixin(LightningElement) {
-
+    map = null;
+    countriesData = [];
     renderedCallback() {
-        alert("2.18")
+        // alert("3.1")
         debugger
         if (this.leaftletjsInitialized) {
             return;
@@ -19,48 +21,62 @@ export default class NcMap extends NavigationMixin(LightningElement) {
             loadStyle(this, LEAFTLET_JS + '/leaflet.css')
         ]).then(() => {
             this.initMap();
-        })
-            .catch(error => {
-                debugger;
-                this.error = error;
-            });
+        }).catch(error => {
+            debugger;
+            this.error = error;
+        });
     }
+
+    @wire(getCountByCountry)
+    getCountByCountry({ data, error }) {
+        if (data) {
+            this.countriesData = data.map(({ Country__c, expr0: count }) => ({
+                country: Country__c.toLowerCase(),
+                title: `${Country__c} (${count} Progetti)`,
+            }));
+            this.setMarkers()
+        }
+    }
+
     initMap() {
         debugger;
         let m = this.template.querySelector('.map');
-        var map = L.map(m, { zoomControl: true, zoom: 1 })
+        this.map = L.map(m, { zoomControl: true, zoom: 1 })
             .setView([41.871941, 12.567380], 1)
-            // .zoomOut().zoomOut().zoomOut();
 
         L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
             {
                 attribution: 'NoeliCloud © 2019'
-            }).addTo(map);
+            }).addTo(this.map);
 
-        // Add marker
-        const marker = L.marker([41.871941, 12.567380])
-        marker.addTo(map)
-            .bindPopup('Home of Dreamforce');
+        this.setMarkers()
 
-
-        marker.on('mouseover', () => {
-            marker.openPopup();
-        });
-
-        marker.on('click', (ev) => {
-            debugger
-            this.navigateToList() // ev is an event object (MouseEvent in this case)
-        });
-
-        const marker2 = L.marker([-14.235004, -51.925282])
-        marker2.addTo(map)
-            .bindPopup('Home of Dreamforce');
-        marker2.on('click', (ev) => {
-            debugger
-            this.navigateToList() // ev is an event object (MouseEvent in this case)
-        });
     }
-    navigateToList() {
+
+    setMarkers() {
+        if (this.map && !this.mapHasMarker && this.countriesData.length) {
+            this.mapHasMarker = true
+
+            this.countriesData.forEach(({ country, title }) => {
+                const countryData = mapData[country]
+                if (countryData) {
+                    // Add marker                
+                    const marker = L.marker([countryData.latitude, countryData.longitude])
+                        .addTo(this.map)
+                        .bindPopup(title)
+                        .on('mouseover', () => {
+                            marker.openPopup();
+                        })
+                        .on('click', () => {
+                            this.navigateToList(countryData.filter) // ev is an event object (MouseEvent in this case)
+                        })
+                }
+
+            })
+        }
+    }
+
+    navigateToList(filterName) {
         this[NavigationMixin.Navigate]({
             type: 'standard__objectPage',
             attributes: {
@@ -68,7 +84,7 @@ export default class NcMap extends NavigationMixin(LightningElement) {
                 actionName: 'list'
             },
             state: {
-                filterName: '00B1i000000ZPJ8EAO'
+                filterName
             },
         });
     }
