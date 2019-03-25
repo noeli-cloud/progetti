17ddd1fcc6ac2c4633ceccc9e11168398325b799
modestemax <modestemax@gmail.com>
2019-03-25 02:26:15 +0100

    ..

--- force-app/main/default/lwc/ncMap/ncMap.js
+++ force-app/main/default/lwc/ncMap/ncMap.js
@@ -1,11 +1,13 @@
 import { LightningElement, track } from 'lwc';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import LEAFTLET_JS from '@salesforce/resourceUrl/leaflet';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class NcMap extends LightningElement {
+
+export default class NcMap extends NavigationMixin(LightningElement) {
 
     renderedCallback() {
-        alert("2.5")
+        alert("2.9")
         debugger
         if (this.leaftletjsInitialized) {
             return;
@@ -28,18 +30,30 @@ export default class NcMap extends LightningElement {
         let m = this.template.querySelector('.map');
         var map = L.map(m, { zoomControl: false }).setView([37.784173, -122.401557], 14);
 
-        // L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
-        //     {
-        //         attribution: 'Tiles © Esri'
-        //     }).addTo(map);
+        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+            {
+                attribution: 'NoeliCloud © 2019'
+            }).addTo(map);
 
         // Add marker
         const marker = L.marker([37.784173, -122.401557])
         marker.addTo(map)
             .bindPopup('Home of Dreamforce');
-        marker.on('click', function (ev) {
+        marker.on('click', (ev) => {
             debugger
-            alert(ev.latlng); // ev is an event object (MouseEvent in this case)
+            this.navigateToList() // ev is an event object (MouseEvent in this case)
+        });
+    }
+    navigateToList() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Progetto__c',
+                actionName: 'list'
+            },
+            state: {
+                filterName: '00B1i000000ZPJ8EAO'
+            },
         });
     }
 }
